ee16555de4f47339c750200e9394aac421346c00
Gustrb <gustavoreisbauer@gmail.com>
2022-03-21 09:17:19 -0300

    Adding better documentation

--- packages/dotenv/dotenv.h
+++ packages/dotenv/dotenv.h
@@ -7,4 +7,8 @@ typedef struct dotenv {
   const char *path;
 } dotenv_instance_t;
 
+// Initializes a dotenv if it is not already initialized.
+// Returns NULL if there is no such prop_name;
+// This is a WIP and it should be changed to use a hashmap as soon as it is
+// implemented.
 char *dotenv_fetch_prop(dotenv_instance_t *dotenv, char *prop_name);
